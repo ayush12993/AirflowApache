9bc63d1e02f048c260b7e3d97d8231c9189adf7f
ayush12993 <ayush2202ksr@gmail.com>
2022-11-30 13:44:13 +0530

    first commit

--- projects/dags/first_dag.py
+++ projects/dags/first_dag.py
@@ -11,6 +11,7 @@ except Exception as e:
 def first_function_execute(*args,**kwargs):
     variable=kwargs.get("name","Didn,t not get the key")
     print("Hello World ".format(variable))
+    print("Comment")
     return "Hello World" + variable    
 
 with DAG(
@@ -24,7 +25,7 @@ with DAG(
             },
         catchup=False) as f:
        
-
+    
     first_function_execute = PythonOperator(
            task_id="first_function_execute",
            python_callable=first_function_execute,
